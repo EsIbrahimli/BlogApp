aef438b86e179a4c36404a3018c027d716b5fc66
Asmar Ibrahimli <ibrahimli.asmar@gmail.com>
2026-02-11 01:58:46 +0400

    created layout system

---
+++ src/pages/Blogs/Blog.jsx
@@ -0,0 +1,14 @@
+import styles from './Blog.module.css'
+import Layouts from '../../components/Layout/Layouts.jsx';
+
+import React from 'react'
+
+const Blog = () => {
+  return (
+ <Layouts>
+    <div>Blogs</div>
+   </Layouts>
+  )
+}
+
+export default Blog
---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+
+import styles from './Contact.module.css'
+import Layouts from '../../components/Layout/Layouts.jsx';
+
+import React from 'react'
+
+const Contact = () => {
+  return (
+  <Layouts>
+    <div>Contact</div>
+   </Layouts>
+  )
+}
+
+export default Contact
